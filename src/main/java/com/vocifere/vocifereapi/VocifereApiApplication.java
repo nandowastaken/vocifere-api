fc82c28e2c3e850d65bb4c62553a065d5b05f694
nandowastaken <fernandojorge.cavalcantegomes@gmail.com>
2024-03-02 14:08:47 -0300

    feat: initialize project

---
+++ src/main/java/com/vocifere/vocifereapi/VocifereApiApplication.java
@@ -0,0 +1,13 @@
+package com.vocifere.vocifereapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocifereApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocifereApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vocifere/vocifereapi/VocifereApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vocifere.vocifereapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocifereApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
